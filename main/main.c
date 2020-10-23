e026c0f3f92672e6dcbbebf3c171501830250a1d
nvmd <phd.pc@hotmail.com>
2020-10-23 17:13:50 -0300

    Removed dead code

--- main/main.c
+++ main/main.c
@@ -24,22 +24,6 @@ static const char *TAG = "main";
 
 EventGroupHandle_t eth_ev;
 
-#if CONFIG_W5100_CUSTOM_SPI_TRANS
-#	include "freertos/semphr.h"
-
-#	include "w5100_spi.h"
-
-SemaphoreHandle_t spi_mutex;
-
-void spi_trans( spi_device_handle_t spi, uint32_t tx, uint32_t *rx )
-{
-	xSemaphoreTake( spi_mutex, portMAX_DELAY );
-	ESP_ERROR_CHECK(
-		spi_device_transmit( spi, &( spi_transaction_t ){ .length = 32, .tx_buffer = &tx, .rx_buffer = rx } ) );
-	xSemaphoreGive( spi_mutex );
-}
-#endif
-
 /** Event handler for Ethernet events */
 static void eth_event_handler( void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data )
 {
@@ -113,11 +97,6 @@ void tasklol( void *p )
 	ESP_ERROR_CHECK( esp_event_handler_register( ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL ) );
 	ESP_ERROR_CHECK( esp_event_handler_register( IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL ) );
 
-#if CONFIG_W5100_CUSTOM_SPI_TRANS
-	ESP_ERROR_CHECK( !( spi_mutex = xSemaphoreCreateMutex() ) );
-
-	set_spi_trans_cb( spi_trans );
-#endif
 	// Uncomment the initializer list below to enable static IPv4
 	eth_main( &( struct eth_ifconfig ){
 		.hostname = "w5100_esp32",
